a2a21ef54c40e5fac4f15c647d540967ac080bb2
Corolaine <contacaroline.coutinho@gmail.com>
2024-06-20 00:12:03 -0300

    Projeto Generation conta bancaria

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,6 +1,7 @@
 package conta;
 import conta.model.Conta;
-
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
@@ -13,6 +14,21 @@ public class Menu {
         c1.depositar(5000.0f);
         c1.visualizar();
 
+        // Teste da Classe Conta Corrente
+        ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+        cc1.visualizar();
+        cc1.sacar(12000.0f);
+        cc1.visualizar();
+        cc1.depositar(5000.0f);
+        cc1.visualizar();
+
+        // Teste da Classe Conta Poupança
+        ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+        cp1.visualizar();
+        cp1.sacar(1000.0f);
+        cp1.visualizar();
+        cp1.depositar(5000.0f);
+        cp1.visualizar();
 
         Scanner leia = new Scanner(System.in);
 
